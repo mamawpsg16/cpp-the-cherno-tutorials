f2585b6ac1aa53e5747e24ee813d41bd1e9e574d
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-12-20 13:59:51 +0800

    update

--- TheChernoC++/TheChernoC++/src/MoveSemantics.cpp
+++ TheChernoC++/TheChernoC++/src/MoveSemantics.cpp
@@ -3,7 +3,7 @@
 
 class String {
 public:
-	String() = default;
+	String() : m_string(nullptr), m_size(0) {}
 	String(const char* string) {
 		std::cout << "CONSTRUCTOR" << '\n';
 		m_size = strlen(string);
@@ -12,6 +12,7 @@ public:
 		m_string[m_size] = 0;
 	}
 
+	//COPY CONSTRUCTOR
 	String(const String& other) {
 		std::cout << "COPY CONSTRUCTOR" << '\n';
 		m_size = other.m_size;
@@ -20,10 +21,44 @@ public:
 		m_string[m_size] = 0;
 	}
 
-	void printString() {
-		std::cout << m_string << '\n';
+	//MOVE CONSTRUCTOR
+	String(String&& other) noexcept {
+		std::cout << "MOVE CONSTRUCTOR" << '\n';
+		m_size = other.m_size;
+		m_string = other.m_string;
+
+		other.m_size = 0;
+		other.m_string = nullptr;
 	}
+
+	//MOVE ASSIGNMENT OPERATOR
+	String& operator =(String&& other) noexcept {
+		std::cout << "MOVE ASSIGNMENT" << '\n';
+
+		if (this != &other) {
+			delete[] m_string;  // Free the existing resource
+
+			m_size = other.m_size;
+			m_string = other.m_string;
+
+			other.m_size = 0;
+			other.m_string = nullptr;
+		}
+
+		return *this;
+	}
+
+	void printString() {
+		if (m_string) {
+			std::cout << m_string << '\n';
+		}
+		else {
+			std::cout << "String is empty" << '\n';
+		}
+	}
+
 	~String() {
+		std::cout << "DESTROYED" << '\n';
 		delete[] m_string;
 	}
 private:
@@ -34,6 +69,11 @@ private:
 class Entity {
 public:
 	Entity(const String& string) : m_string(string) {
+
+	}
+
+	//Entity(String&& string) : m_string((String&&)string) {
+	Entity(String&& string) : m_string(std::move(string)) {
 
 	}
 
@@ -48,7 +88,29 @@ int main() {
 	/*String str("Kevin Mensah");
 	str.printString();*/
 
-	Entity entity(String("Kevin Mensah"));
-	entity.print();
+	// MOVE CONSTRUCTOR
+	//Entity entity = String("Kevin Mensah");
+	/*Entity entity("Kevin Mensah"); 
+	entity.print();*/
 
+	// MOVE ASSIGNMENT
+	
+	/* THIS TRIGGERS MOVE CONSTRUCTOR INSTEAD AS THE entity1 is not already an object*/
+	//Entity entity1 = (String&&)entity;
+	//Entity entity1 = std::move(entity);
+
+	/* MOVE ASSIGNMENT OPERATOR */
+	String apple = "APPLE";
+	String dest; 
+	std::cout << "Apple :" << '\n';
+	apple.printString();
+	std::cout << "Dest :" << '\n';
+	dest.printString();
+
+	dest = std::move(apple);
+
+	std::cout << "Apple :" << '\n';
+	apple.printString();
+	std::cout << "Dest :" << '\n';
+	dest.printString();
 }
